e90aa4f5942332d76b867c81be306730f4647bfc
Microwaved-Pie <144716146+Microwaved-Pie@users.noreply.github.com>
2024-04-24 18:26:41 +0000

    ef

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-console.log("AHHHHHHHHHHHHHHHHHHHHHHHHH");
+console.log("Task 1");
+let i = 1;
+
+while   (i < 10)   {
+    console.log(i*i)
+    let i = i++;
+}
+console.log("Task 2");
+
+console.log("Task 3");
+
+console.log("Task 4");
+
+console.log("Task 5");
