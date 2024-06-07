546973ee6116e6d399d6038cd0d1bb584f1c1349
Microwaved-Pie <144716146+Microwaved-Pie@users.noreply.github.com>
2024-06-07 19:32:08 +0000

    made everything. so. so much worse.

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,5 @@
-console.log("Task 1");
-for (let i = 1; i < 11; i++) {
-    console.log(i * i);
-  }
-console.log("Task 2");
-for (let i = 5; i > 0; i--) {
-    console.log(i)
-}
-console.log("Blast Off!")
-console.log("Task 3");
-for (let i = 2; i < 51; i+=2) {
-    console.log(i)
-}
+let curtain = document.getElementById("curtain");
 
- let sum = 0
-console.log("Task 4");
-for (let i = 1; i < 101; i++) {
-    sum = i + sum 
-}
-console.log(sum)
-
-sum = 1
-let factorial = 0
-console.log("Task 5");
-for (let i = 1; i < 11; i++) {
-    factorial++
-    sum = sum * factorial
-    console.log(sum)
-}
-console.log("Task 6");
-let arr = [3, 1, 4, 1, 5]
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr.at(i))
-}
-console.log("Task 7");
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr.at(-i-1))
-}
-console.log("Task 8");
-let arr2 = []
-for (let i = 1; i < 11; i++) {
-    arr2.push(i*i*i)
-
-}
-console.log(arr2)
-console.log("Task 9");
-
-console.log("Task 10");
+curtain.addEventListener("click", function(){
+    curtain.hidden = true;
+})
